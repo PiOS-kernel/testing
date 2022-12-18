140ed47280e8a17cf597884951bd0908d6d4cfb0
AlbertoZaupa <alberto.zaupa@studenti.unitn.it>
2022-12-18 18:08:11 +0100

    added tests module

--- src/main.c
+++ src/main.c
@@ -1,18 +1,19 @@
 #include "../includes/_common.h"
 #include <stdint.h>
+#include "tests/tests.h"
 
 //main - va avanti all'infinito
 int main ( void )
 {    
-    //serial_print("before\n");
-    //__asm__("SVC #01\n\t");
-    serial_print("after\n");
-
+    test_runner();
     while(1);
     return(0);
 }
 
+
+
 void HardFaultISR()
 {
+    serial_print("HardFault, baby!\n");
 	while(1);
 }
