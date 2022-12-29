e25796ba9258b3cb860a802cda5544b80599efb2
AlbertoZaupa <alberto.zaupa@studenti.unitn.it>
2022-12-29 16:36:18 +0100

    linked with cortex-m3 branch on 'Kernel' and added integration tests

---
+++ integration/int_runner.c
@@ -0,0 +1,54 @@
+#include "integration.h"
+#include "../tests.h"
+#include "../../includes/tools.h"
+
+RunnerState runner_state = {0, 0, 0};
+int scheduler_state = 0;
+int scheduler_running = 0;
+bool test_result = false; 
+extern void summon_scheduler();
+
+extern bool test_producer_consumer();
+
+Test tests[] = {
+    {"test_producer_consumer", test_producer_consumer},
+};
+
+void integration_tests_runner() {
+    while(1);
+    int tests_count = sizeof(tests)/sizeof(Test);
+
+    int passed = 0;
+    int failed = 0;
+    serial_print("\nRunning integration tests...\n");
+
+    for (int i=0; i<tests_count; ++i) {
+        // The kernel is initialized before each test to reset the heap
+        // and the tasks queues.
+        kernel_init();
+        
+        serial_print(tests[i].name);
+        serial_print(" ... ");
+        tests[i].test();
+        summon_scheduler();
+
+        if (test_result) {
+            serial_println("[ ok ]");
+            passed++;
+        } else {
+            serial_println("[ failed ]");
+            failed++;
+        }
+
+        test_result = false;
+    }
+
+    char test_passed[10];
+    char test_failed[10];
+    itoa(passed, test_passed);
+    itoa(failed, test_failed);
+    serial_print("\n\nTests passed: ");
+    serial_print(test_passed);
+    serial_print("\nTests failed: ");
+    serial_println(test_failed);
+}
---
+++ integration/scheduling.c
@@ -0,0 +1,61 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "../../pios-kernel/kernel/kernel.h"
+#include "../../includes/tools.h"
+#include "integration.h"
+
+typedef struct PublishedData {
+    uint32_t data;
+    bool consumed;
+} PublishedData;
+
+bool producer_running = true;
+bool consumer_running = true;
+PublishedData data = {0, true};
+
+void producer_task(PublishedData* data) {
+    for (int i=0; i<100; ++i) {
+        // waits for the consumer task to consume data
+        while (!data->consumed);
+
+        data->data = i;
+        data->consumed = false;
+    }
+    
+    producer_running = false;
+    while(1); // the task waits forever
+}
+
+void consumer_task(PublishedData* data) {
+    for (int i=0; i<100; ++i) {
+        // waits for the producer task to publish data
+        while (data->consumed);
+
+        if (data->data != i) {
+            test_result = false;
+            while(1); // the task waits forever
+        }
+        data->consumed = true;
+    }
+
+    test_result = true;
+    producer_running = false;
+    while(1); // the task waits forever
+}
+
+// The task checks wether both the producer and the consumer are finshed and 
+// deactivates the scheduler
+
+void checker_task() {
+    while (producer_running || consumer_running);
+
+    // The scheduler is deactivated
+    scheduler_state = 0;
+}
+
+bool test_producer_consumer() {
+    // create the tasks
+    kcreate_task(producer_task, (void*)&data, 0);
+    kcreate_task(consumer_task, (void*)&data, 0);
+    kcreate_task(checker_task, NULL, 0);
+}
