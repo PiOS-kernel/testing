793901f91039edc0232cdf8d1584700516fd3617
AlbertoZaupa <alberto.zaupa@studenti.unitn.it>
2023-01-19 22:07:29 +0100

    fixed compilation errors and renamed file to _pipes.c

--- testing/_pipes.c
+++ testing/_pipes.c
@@ -1,41 +1,41 @@
-#include "../pios-kernel/kernel/synch/pipe.h"
+#include "../pios-kernel/kernel/kernel.h"
 #include <stdbool.h>
 #include <stdint.h>
 
 extern EventHandle test_completed_event;
 
-void writer_task1(PIPE pipe){
+void writer_task1(PIPE* pipe){
     int msg=0;
-    pub_msg(pipe, msg);
+    pub_msg(pipe, (void*) &msg);
     //writes a single message in the pipe 
     task_exit();
 }
 
-void writer_task2(PIPE pipe){
+void writer_task2(PIPE* pipe){
     int msg1=1;
-    pub_msg(pipe, msg);
-    char msg2=2;
-    pub_msg(pipe, msg);
+    pub_msg(pipe, (void*) &msg1);
+    int msg2=2;
+    pub_msg(pipe, (void*) &msg2);
     //writes two messages in the pipe
     task_exit();
 }
 
-void writer_task3(PIPE pipe){
+void writer_task3(PIPE* pipe){
     int msg1=3;
-    pub_msg(pipe, msg);
+    pub_msg(pipe, (void*) &msg1);
     int msg2=4;
-    pub_msg(pipe, msg);
+    pub_msg(pipe, (void*) &msg2);
     //writes two messages in the pipe
     task_exit();
 }
 
-void reader_task1(PIPE pipe){
-    uint32_t test_result = 1;
+void reader_task1(PIPE* pipe){
+    bool test_result = true;
     int msg;
     for(int i=0; i<3; i++){
-        read_msg(pipe, msg);
-        if(msg != 0 | msg != 1 | msg != 2 | msg != 3 | msg != 4 ){
-            test_result = 0;
+        read_msg(pipe, (void*) &msg);
+        if(msg != 0 && msg != 1 && msg != 2 && msg != 3 && msg != 4 ){
+            test_result = false;
             event_post(test_completed_event, &test_result); //message not valid
         }else if(msg == 4){
             event_post(test_completed_event, &test_result); //read last message
@@ -46,13 +46,13 @@ void reader_task1(PIPE pipe){
     task_exit();
 }
 
-void reader_task2(PIPE pipe){
-    uint32_t test_result = 1;
+void reader_task2(PIPE* pipe){
+    bool test_result = true;
     int msg;
-    while(int i=0; i<2; i++){
-        read_msg(pipe, msg);
-        if(msg != 0 | msg != 1 | msg != 2 | msg != 3 | msg != 4 ){
-            test_result = 0;
+    for(int i=0; i<2; i++){
+        read_msg(pipe, (void*) &msg);
+        if(msg != 0 && msg != 1 && msg != 2 && msg != 3 && msg != 4 ){
+            test_result = false;
             event_post(test_completed_event, &test_result); //message not valid
         }else if(msg == 4){
             event_post(test_completed_event, &test_result); //read last message
@@ -63,7 +63,7 @@ void reader_task2(PIPE pipe){
 }
 
 void test_pipe(){
-    PIPE pipe = NEW_PIPE(4,int);
+    PIPE* pipe = NEW_PIPE(4,int);
 
     //create the tasks
     create_task((void(*)(void*)) reader_task1, (void*)pipe, 0, NULL);
